fe6ec8d1c3beeef99d1248a397a38422b9705667
Melvolio01 <andyhunt353@gmail.com>
2021-03-14 17:27:49 +0000

    Add static listings page

--- django_places/settings.py
+++ django_places/settings.py
@@ -33,6 +33,8 @@ ALLOWED_HOSTS = []
 
 INSTALLED_APPS = [
     'pages.apps.PagesConfig',
+    'listings.apps.ListingsConfig',
+    'agents.apps.AgentsConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
