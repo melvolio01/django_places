950f8990d224e06afc91b8238074bca392c79bfe
Melvolio01 <andyhunt353@gmail.com>
2021-03-22 21:45:52 +0000

    Update admin menus

--- agents/admin.py
+++ agents/admin.py
@@ -1,5 +1,9 @@
 from django.contrib import admin
 from .models import Agent
 
-# Register your models here.
-admin.site.register(Agent)
+class AgentAdmin(admin.ModelAdmin):
+    list_display = ('id', 'name', 'email', 'hired_on')
+    list_display_links = ('id', 'name')
+    list_per_page = 10
+
+admin.site.register(Agent, AgentAdmin)
